56093a3614383c794347cd0498395840255ca4e2
wohugb@aliyun.com <wohugb@qq.com>
2023-07-18 10:00:35 +0800

    merge

---
+++ src/extension.ts
@@ -0,0 +1,30 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import { Translate } from "./translate";
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  let out: vscode.OutputChannel;
+  let disposable = vscode.commands.registerCommand("extension.youdao", () => {
+    if (!out) {
+      out = vscode.window.createOutputChannel("有道翻译");
+      out.show();
+    }
+    new Translate(out).translate();
+  });
+  // 插入替换指令
+  context.subscriptions.push(disposable);
+
+  //   let disposable2 = vscode.commands.registerCommand("extension.translateReplace", () => {
+  //     let editor: any = vscode.window.activeTextEditor;
+  //     let selection = editor.selection;
+  //     editor.edit((edit: any) => edit.replace(selection, translateResult));
+  //   });
+
+  //   context.subscriptions.push(disposable2);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
